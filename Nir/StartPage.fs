58e85503d8c1c9179ef13d874702e3b0f07cbfc3
Renngar <renngar@renngar.com>
2020-07-08 19:05:10 -0400

    Place an Advanced... link in the lower right corner of the start page

--- Nir/StartPage.fs
+++ Nir/StartPage.fs
@@ -18,16 +18,31 @@ module StartPage =
 
     let update (_: Msg) (state: State) =
         state
+
+    let textBlockAttrs row cls text = [
+        Grid.row row
+        TextBlock.classes [ cls ]
+        TextBlock.text text ]
+        
+    let textBlock row cls text =
+        TextBlock.create
+        <| textBlockAttrs row cls text 
+    
+    let textBlockEx row cls text rest  =
+        TextBlock.create
+        <| List.append (textBlockAttrs row cls text) rest
         
     let view (_: State) (_: Msg -> unit) =
-        DockPanel.create [
-            DockPanel.horizontalAlignment HorizontalAlignment.Center
-            DockPanel.verticalAlignment VerticalAlignment.Top
-            DockPanel.children [
-                TextBlock.create [
-                    TextBlock.classes [ "title" ]
-                    TextBlock.text "Nir" ] ] ]
-        
+        Grid.create [
+            Grid.margin 10.0
+            Grid.rowDefinitions "auto, auto, *, auto"
+            Grid.children [
+                textBlock 0 "title" "Nir"
+                textBlock 1 "subtitle" "Nir lets you install Skyrim Mod Guides from the Web"
+                textBlock 2 "" ""
+                textBlockEx 3 "link" "Advanced..." [
+                    TextBlock.horizontalAlignment HorizontalAlignment.Right
+                    TextBlock.verticalAlignment VerticalAlignment.Stretch ] ] ]
 
     type Host() as this =
         inherit Hosts.HostControl()
