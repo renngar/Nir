9d9f886e16ce12ac501e2cfae751b759f0d82f85
Renngar <renngar@renngar.com>
2020-08-02 18:03:47 -0400

    Use rcedit to add the icon to the .exe
    This makes the app look right in Explorer.

--- publish.cmd
+++ publish.cmd
@@ -1,4 +1,5 @@
 cd Nir
 mkdir ..\Published
 dotnet publish -c Release --self-contained -r win-x86 -o ..\Published\Nir
+..\Tools\rcedit-x86.exe ..\Published\Nir\Nir.exe --set-icon Assets\Icons\Nir.ico
 cd ..
