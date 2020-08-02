a7a891990b62bf043d266ad44377b290a76239d3
Renngar <renngar@renngar.com>
2020-08-02 08:43:40 -0400

    Find the correct path for a published application

--- Nir.Core/Utility/Path.fs
+++ Nir.Core/Utility/Path.fs
@@ -3,15 +3,20 @@
 open System.Text.RegularExpressions
 
 /// Custom operator for combining paths
-let ( +/ ) path1 path2 = System.IO.Path.Combine(path1, path2)
+let (+/) path1 path2 = System.IO.Path.Combine(path1, path2)
 
 /// Returns the root directory of the currently running program
 ///
 /// For example if the executable is in C:\Program\bin\Debug\netcoreapp3.1, this
 /// will return C:\Program.
-let getProgramPath () =
+let getProgramPath() =
+    let tryMatch rx path =
+        let m = Regex.Match(path, rx)
+        if m.Success then m.Groups.[1].Value else path
+
     let root =
         System.Reflection.Assembly.GetExecutingAssembly().CodeBase
-        |> (fun path -> Regex.Match(path, @"^(.*)/bin").Groups.[1].Value)
-    let uri = System.Uri(root)
-    uri.LocalPath
+        |> tryMatch @"^(.*)/bin" // Development Build
+        |> tryMatch @"^(.*)/[^/]*\.(exe|dll)" // Published Release
+
+    System.Uri(root).LocalPath
