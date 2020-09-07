1cd2171228ade87625a22cb6d59f15ca441e17aa
Renngar <renngar@renngar.com>
2020-09-07 16:49:51 -0400

    Replace publish.cmd with a FAKE task

--- build.fsx
+++ build.fsx
@@ -1,17 +1,21 @@
-open Fake.DotNet
-#r "paket:
-nuget Fake.DotNet.Cli
-nuget Fake.IO.FileSystem
-nuget Fake.Core.Target //"
-#load ".fake/build.fsx/intellisense.fsx"
+open System.Diagnostics
 open System.IO
+open System.Runtime.InteropServices
 open Fake.Core
 open Fake.DotNet
 open Fake.IO
 open Fake.IO.Globbing.Operators
 open Fake.Core.TargetOperators
+#r "paket:
+nuget Fake.DotNet.Cli
+nuget Fake.IO.FileSystem
+nuget Fake.Core.Target //"
+#load ".fake/build.fsx/intellisense.fsx"
 
-let solution =  Path.Combine(Directory.GetCurrentDirectory(), "Nir.sln")
+let getPath x = Path.Combine(__SOURCE_DIRECTORY__, x)
+let solution = getPath "Nir.sln"
+
+let dotnet cmd args = DotNet.exec id cmd args |> ignore
 
 Target.initEnvironment ()
 
@@ -28,9 +32,7 @@ Target.create "Build" (fun _ ->
 )
 
 Target.create "Lint" (fun _ ->
-    DotNet.exec id "fsharplint" 
-    <| "-f msbuild lint --lint-config fsharplint.json " + solution
-    |> ignore
+    dotnet "fsharplint" ("-f msbuild lint --lint-config fsharplint.json " + solution)
 )
 
 Target.create "Test" (fun _ ->
@@ -40,10 +42,28 @@ Target.create "Test" (fun _ ->
 
 Target.create "All" ignore
 
+Target.create "Publish" (fun _ ->
+    Directory.SetCurrentDirectory(getPath "src/Nir")
+    let isWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
+    let rid = if isWindows then "win-x86"
+              elif RuntimeInformation.IsOSPlatform(OSPlatform.Linux) then "linux-x86"
+              elif RuntimeInformation.IsOSPlatform(OSPlatform.OSX) then "osx-x64"
+              else failwith "Unsupported OS platform"
+    dotnet "publish" (sprintf @"-c Release --self-contained -r %s -o ..\..\Published\Nir" rid)
+    if isWindows then
+        Process.Start(@"..\..\Tools\rcedit-x86.exe",
+                      @"..\..\Published\Nir\Nir.exe --set-icon Assets\Icons\Nir.ico").WaitForExit()
+    Directory.SetCurrentDirectory(getPath "Plugins/ModChecker")
+    dotnet "publish" (sprintf @"-c Release -r %s -o ..\..\Published\Nir\Plugins\ModChecker" rid)
+)
+
 "Clean"
   ==> "Build"
   ==> "Lint"
   ==> "Test"
   ==> "All"
+
+"All"
+  ==> "Publish"
 
 Target.runOrDefault "Build"
