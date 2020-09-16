6d360da2f588619ad34af588032f4f6ab6a93b80
Renngar <renngar@renngar.com>
2020-09-16 19:18:32 -0400

    Move the ModInfo display into a separate component

--- Plugins/ModChecker/ModChecker.fs
+++ Plugins/ModChecker/ModChecker.fs
@@ -1,9 +1,8 @@
 module Nir.Plugins.ModChecker
 
-open Avalonia.Controls
-open FSharp.Data
-
+open System
 open Elmish
+open Avalonia.Controls
 open Avalonia.FuncUI.Components
 open Avalonia.FuncUI.DSL
 open Avalonia.FuncUI.Types
@@ -16,6 +15,8 @@ open Nir.DSL // FuncUI DragDrop support
 open Nir.NexusApi
 open Nir.UI
 
+open ModChecker
+
 // TODO: Tell what game domain the mod was found in.
 // TODO: Support searching for multiple mods at the same time.
 // TODO: Consider using the error messages returned by Nexus.
@@ -25,34 +26,10 @@ type private Msg =
     | GotGames of ApiResult<Game []>
     | GameChanged of int
     | OpenFileDialog
-    | ChangeFile of string []
-    | SelectionChanged of string []
-    | CheckFile of string
-    | MD5Progress of int64 * int64
-    | MD5Complete of Result<string, string>
-    | SearchResult of string list * ApiResult<Md5Search []>
-
-module private Sub =
-    let md5Search file onProgress onComplete dispatch =
-        async {
-            async {
-                try
-                    Nir.Utility.Md5sum.md5sum file (onProgress >> dispatch) |> Ok
-                with e -> e.Message |> Error
-                |> (onComplete >> dispatch)
-            }
-            |> Async.Start
-        }
-        |> Async.RunSynchronously
+    | ChangeFileSelection of string []
+    | ModInfoMsg of ModInfo.Msg
 
 // Model
-
-type private ArchiveState =
-    | None
-    | Hashing
-    | Checking
-    | Found of Md5Search []
-    | NotFound of ApiError
 
 type private Model =
     { Window: Window
@@ -60,11 +37,7 @@ type private Model =
       Games: Game []
       GamesByName: Map<string, Game>
       SelectedGames: Game list
-      Archive: string
-      Hash: string
-      State: ArchiveState
-      ProgressCurrent: int64
-      ProgressMax: int64 }
+      ModInfo: ModInfo.Model list }
 
 let private init window nexus =
     { Window = window
@@ -72,55 +45,13 @@ let private init window nexus =
       Games = [||]
       GamesByName = Map.empty
       SelectedGames = []
-      Archive = ""
-      Hash = ""
-      State = None
-      ProgressCurrent = 0L
-      ProgressMax = 0L }, Cmd.ofMsg FetchGames
+      ModInfo = [] }, Cmd.ofMsg FetchGames
 
 // Update
 
-let private searchInDomains model gameDomains notFoundModel =
-    let search model gameDomain remainingDomains =
-        { model with State = Checking },
-        Cmd.OfAsync.perform md5Search (model.Nexus, gameDomain, model.Hash)
-            (fun result -> SearchResult(remainingDomains, result))
-
-    match Seq.toList gameDomains with
-    | [ d ] -> search model d []
-    | d :: rest -> search model d rest
-    | [] -> notFoundModel, Cmd.none
+let inline private processingFile model = Seq.exists ModInfo.processingFile model.ModInfo
 
 let private update (msg: Msg) (model: Model): Model * Cmd<_> =
-    let checkHash model =
-        let domains = Seq.map (fun (g: Game) -> g.DomainName) model.SelectedGames
-        searchInDomains model domains model
-
-    let maybeCheckFile model games =
-        // If SSE or FoNV are selected, also search the older games whose mods may be used.
-        let sle = "Skyrim"
-        let sse = "Skyrim Special Edition"
-        let fonv = "Fallout New Vegas"
-        let fo3 = "Fallout 3"
-
-        let gs =
-            match games with
-            | [ (game: Game) ] ->
-                let findGames = List.map (fun name -> model.GamesByName.[name])
-                if game.Name = sse then findGames [ sse; sle ]
-                elif game.Name = fonv then findGames [ fonv; fo3 ]
-                else games
-            | _ -> games
-
-        let gameChanged = gs <> model.SelectedGames
-
-        let newModel =
-            if gameChanged then { model with SelectedGames = gs } else model
-
-        if model.Archive.Length = 0 then newModel, Cmd.none
-        elif not gameChanged || model.Hash.Length = 0 then newModel, Cmd.ofMsg (CheckFile model.Archive)
-        else checkHash newModel
-
     match msg with
     | FetchGames -> model, Cmd.OfAsync.perform games (model.Nexus, false) GotGames
     | GotGames games ->
@@ -136,72 +67,35 @@ let private update (msg: Msg) (model: Model): Model * Cmd<_> =
         | Error _ -> model, Cmd.none
     | GameChanged n ->
         if n >= 0 then
-            let game = model.Games.[n]
-            if game.Name = "Skyrim Special Edition" then
-                List.append [ game ]
-                    [ for g in model.Games do
-                        if g.Name = "Skyrim" then yield g ]
-            else
-                [ game ]
-            |> maybeCheckFile model
+            // If SSE or FoNV are selected, also search the older games whose mods may be used.
+            let sle = "Skyrim"
+            let sse = "Skyrim Special Edition"
+            let fonv = "Fallout New Vegas"
+            let fo3 = "Fallout 3"
+            let findGames = List.map (fun name -> model.GamesByName.[name])
+
+            let gs =
+                let game = model.Games.[n]
+                if game.Name = sse then findGames [ sse; sle ]
+                elif game.Name = fonv then findGames [ fonv; fo3 ]
+                else [ game ]
+            { model with SelectedGames = gs }, Cmd.none
         else
             model, Cmd.none
-    | OpenFileDialog -> model, Cmd.OfAsync.perform promptModArchive model.Window ChangeFile
-    | ChangeFile fileNames ->
-        // This will trigger SelectionChanged when the view updates the TextBlock
-        { model with
-              Archive = Seq.head fileNames
-              State = None }, Cmd.none
-    | SelectionChanged fileNames ->
-        if model.State = Hashing then
+    | OpenFileDialog -> model, Cmd.OfAsync.perform promptModArchive model.Window ChangeFileSelection
+    | ChangeFileSelection fileNames ->
+        if processingFile model || (fileNames.Length = 1 && fileNames.[0] = "") then
             model, Cmd.none
         else
-            maybeCheckFile
-                { model with
-                      Archive = fileNames.[0]
-                      State = None } model.SelectedGames
-    | CheckFile file ->
-        { model with
-              Archive = file
-              State = Hashing }, Cmd.ofSub (Sub.md5Search file MD5Progress MD5Complete)
-    | MD5Progress(current, max) ->
-        { model with
-              ProgressCurrent = current
-              ProgressMax = max }, Cmd.none
-    | MD5Complete r ->
-        match r with
-        | Ok hash -> { model with Hash = hash } |> checkHash
-        | Error e ->
-            { model with
-                  State =
-                      NotFound
-                          { StatusCode = -1
-                            Message = e } }, Cmd.none
-    | SearchResult(gameDomains, r) ->
-        match r with
-        | Ok s ->
-            { model with
-                  Nexus = s.Nexus
-                  State = Found s.Result }, Cmd.none
-        | Error e -> searchInDomains model gameDomains { model with State = NotFound e }
+            let mi = Seq.map (ModInfo.init model.Nexus model.SelectedGames) fileNames |> Seq.toList
+            { model with ModInfo = List.map fst mi }, Cmd.batch (List.map (snd >> Cmd.map ModInfoMsg) mi)
+    | ModInfoMsg miMsg ->
+        let miModel, miCmd = ModInfo.update miMsg model.ModInfo.Head
+        { model with ModInfo = [ miModel ] }, Cmd.map ModInfoMsg miCmd
+
 
 // View
 
-let private titleAndSub title subtitle: seq<IView> =
-    seq {
-        yield TextBlock.create
-                  [ TextBlock.classes [ "h1" ]
-                    TextBlock.dock Dock.Top
-                    TextBlock.text title ]
-
-        yield TextBlock.create
-                  [ TextBlock.classes [ "h2" ]
-                    TextBlock.dock Dock.Top
-                    TextBlock.textWrapping TextWrapping.Wrap
-                    TextBlock.text subtitle ]
-    }
-
-let inline private processingFile model = model.State = Hashing || model.State = Checking
 let inline private isGameSelected model = not model.SelectedGames.IsEmpty
 
 let private gameSelector model dispatch =
@@ -250,21 +144,23 @@ let private modSelector model dispatch =
                         if e.Data.Contains(DataFormats.FileNames) then
                             e.Data.GetFileNames()
                             |> Seq.toArray
-                            |> ChangeFile
+                            |> ChangeFileSelection
                             |> dispatch)
                     TextBox.textWrapping TextWrapping.Wrap
                     TextBox.watermark "Mod archive to verify"
-                    // TextBox.height 30.0
                     TextBox.verticalAlignment VerticalAlignment.Center
                     TextBox.acceptsReturn false
                     TextBox.acceptsTab false
                     TextBox.isEnabled notProcessingFile
                     // This is tacky, but Ctrl-Insert does not work with Avalonia
                     TextBox.tip (ToolTip.create [ ToolTip.content [ "Ctrl-V to paste" ] ])
-                    TextBox.text model.Archive
+                    TextBox.text
+                        (model.ModInfo
+                         |> Seq.map (fun mi -> mi.Archive)
+                         |> String.concat Environment.NewLine)
                     TextBox.onTextChanged (fun s ->
-                        [| s |]
-                        |> SelectionChanged
+                        s.Split Environment.NewLine
+                        |> ChangeFileSelection
                         |> dispatch) ]
                 Button.create
                     [ Grid.column 1
@@ -275,35 +171,6 @@ let private modSelector model dispatch =
                       Button.onClick (fun _ -> dispatch OpenFileDialog)
                       Button.content "Browse..." ] ] ]
 
-let private modInfo model: IView list =
-    match model.State with
-        | None -> []
-        | Hashing ->
-            [ ProgressBar.create
-                [ ProgressBar.dock Dock.Top
-                  ProgressBar.maximum (double model.ProgressMax)
-                  ProgressBar.value (double model.ProgressCurrent) ] ]
-        | Checking ->
-            [ ProgressBar.create
-                [ ProgressBar.dock Dock.Top
-                  ProgressBar.isIndeterminate true ] ]
-        | Found rs ->
-            let r = rs.[0]
-            [ yield! titleAndSub r.Mod.Name r.Mod.Summary
-
-              TextBlock.create
-                  [ TextBlock.dock Dock.Top
-                    TextBlock.fontWeight FontWeight.Bold
-                    TextBlock.text r.FileDetails.Name ] ]
-        | NotFound { StatusCode = code; Message = msg } ->
-            [ TextBlock.create
-                [ TextBlock.dock Dock.Top
-                  TextBlock.classes [ "error" ]
-                  TextBlock.text
-                      (match code with
-                       | HttpStatusCodes.NotFound -> "Unrecognized or Corrupted Archive"
-                       | _ -> msg) ] ]
- 
 let private view (model: Model) (dispatch: Dispatch<Msg>): IView =
     let rowDefs =
         if model.Games.Length = 0 || isGameSelected model then
@@ -316,9 +183,8 @@ let private view (model: Model) (dispatch: Dispatch<Msg>): IView =
             defs
 
     let (contents: IView list) =
-        [ yield! titleAndSub "Nexus Mod Checker"
-                     (if model.State = Hashing then "Generating file hash. Please wait..."
-                      elif model.State = Checking then "Checking with Nexus..."
+        [ yield! ModInfo.titleAndSub "Nexus Mod Checker"
+                     (if processingFile model then "Processing files. Please wait..."
                       elif model.Games.Length = 0 then "Fetching games from Nexus..."
                       elif isGameSelected model then "Drop a mod archive below to verify its contents"
                       else "Select your game below")
@@ -336,13 +202,13 @@ let private view (model: Model) (dispatch: Dispatch<Msg>): IView =
                      else
                          [ gameSelector model dispatch
                            modSelector model dispatch
-                           StackPanel.create
-                               [ Grid.columnSpan 2
-                                 Grid.row 1
-                                 StackPanel.spacing 8.0
-                                 StackPanel.margin (0.0, 8.0)
-                                 StackPanel.children
-                                     (modInfo model)] ]) ] ]
+                           if not <| model.ModInfo.IsEmpty then
+                               StackPanel.create
+                                   [ Grid.columnSpan 2
+                                     Grid.row 1
+                                     StackPanel.spacing 8.0
+                                     StackPanel.margin (0.0, 8.0)
+                                     StackPanel.children [ ModInfo.view model.ModInfo.Head (ModInfoMsg >> dispatch) ] ] ]) ] ]
 
     DockPanel.create
         [ DockPanel.margin 10.0
---
+++ Plugins/ModChecker/ModInfo.fs
@@ -0,0 +1,136 @@
+﻿module ModChecker.ModInfo
+
+open FSharp.Data
+open Elmish
+open Avalonia.Controls
+open Avalonia.FuncUI.DSL
+open Avalonia.FuncUI.Types
+open Avalonia.Media
+
+// FuncUI DragDrop support
+open Nir.NexusApi
+
+type Msg =
+    | CheckFile
+    | MD5Progress of int64 * int64
+    | MD5Complete of Result<string, string>
+    | SearchResult of string list * ApiResult<Md5Search []>
+
+type ArchiveState =
+    | None
+    | Hashing
+    | Checking
+    | Found of Md5Search []
+    | NotFound of ApiError
+
+type Model =
+    { Nexus: Nexus
+      SelectedGames: Game list
+      Archive: string
+      Hash: string
+      State: ArchiveState
+      ProgressCurrent: int64
+      ProgressMax: int64 }
+
+let processingFile model = model.State = Hashing || model.State = Checking
+
+let init nexus selectedGames file =
+    { Nexus = nexus
+      SelectedGames = selectedGames
+      Archive = file
+      Hash = ""
+      State = None
+      ProgressCurrent = 0L
+      ProgressMax = 0L }, Cmd.ofMsg CheckFile
+
+let private searchInDomains model gameDomains notFoundModel =
+    let search model gameDomain remainingDomains =
+        { model with State = Checking },
+        Cmd.OfAsync.perform md5Search (model.Nexus, gameDomain, model.Hash)
+            (fun result -> SearchResult(remainingDomains, result))
+
+    match Seq.toList gameDomains with
+    | [ d ] -> search model d []
+    | d :: rest -> search model d rest
+    | [] -> notFoundModel, Cmd.none
+
+let private checkHash model =
+    let domains = Seq.map (fun (g: Game) -> g.DomainName) model.SelectedGames
+    searchInDomains model domains model
+
+let titleAndSub title subtitle: seq<IView> =
+    seq {
+        yield TextBlock.create
+                  [ TextBlock.classes [ "h1" ]
+                    TextBlock.dock Dock.Top
+                    TextBlock.text title ]
+
+        yield TextBlock.create
+                  [ TextBlock.classes [ "h2" ]
+                    TextBlock.dock Dock.Top
+                    TextBlock.textWrapping TextWrapping.Wrap
+                    TextBlock.text subtitle ]
+    }
+
+
+module private Sub =
+    let md5Search file onProgress onComplete dispatch =
+        async {
+            async {
+                try
+                    Nir.Utility.Md5sum.md5sum file (onProgress >> dispatch) |> Ok
+                with e -> e.Message |> Error
+                |> (onComplete >> dispatch)
+            }
+            |> Async.Start
+        }
+        |> Async.RunSynchronously
+
+let update msg (model: Model) =
+    match msg with
+    | CheckFile -> { model with State = Hashing }, Cmd.ofSub (Sub.md5Search model.Archive MD5Progress MD5Complete)
+    | MD5Progress(current, max) ->
+        { model with
+              ProgressCurrent = current
+              ProgressMax = max }, Cmd.none
+    | MD5Complete r ->
+        match r with
+        | Ok hash -> { model with Hash = hash } |> checkHash
+        | Error e ->
+            { model with
+                  State =
+                      NotFound
+                          { StatusCode = -1
+                            Message = e } }, Cmd.none
+    | SearchResult(gameDomains, r) ->
+        match r with
+        | Ok s ->
+            { model with
+                  Nexus = s.Nexus
+                  State = Found s.Result }, Cmd.none
+        | Error e -> searchInDomains model gameDomains { model with State = NotFound e }
+
+let view model (_: Dispatch<Msg>): IView =
+    let contents: IView list =
+        match model.State with
+        | None -> []
+        | Hashing ->
+            [ ProgressBar.create
+                [ ProgressBar.maximum (double model.ProgressMax)
+                  ProgressBar.value (double model.ProgressCurrent) ] ]
+        | Checking -> [ ProgressBar.create [ ProgressBar.isIndeterminate true ] ]
+        | Found rs ->
+            let r = rs.[0]
+            [ yield! titleAndSub r.Mod.Name r.Mod.Summary
+
+              TextBlock.create
+                  [ TextBlock.fontWeight FontWeight.Bold
+                    TextBlock.text r.FileDetails.Name ] ]
+        | NotFound { StatusCode = code; Message = msg } ->
+            [ TextBlock.create
+                [ TextBlock.classes [ "error" ]
+                  TextBlock.text
+                      (match code with
+                       | HttpStatusCodes.NotFound -> "Unrecognized or Corrupted Archive"
+                       | _ -> msg) ] ]
+    DockPanel.create [ if not <| contents.IsEmpty then DockPanel.children contents ] :> IView
