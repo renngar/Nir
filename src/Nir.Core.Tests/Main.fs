d789fef85265fd1c6ba2d19140dd2ee65bc26b16
Renngar <renngar@renngar.com>
2020-11-22 18:27:28 -0500

    Fixup formatting in a couple files

--- src/Nir.Core.Tests/Main.fs
+++ src/Nir.Core.Tests/Main.fs
@@ -19,16 +19,13 @@
 namespace Nir.Core.Tests
 
 open FParsec.CharParsers
-open FsCheck
-open Nir.Tests
-open Nir.Tests.BBCode
 open Nir.Utility.BBCode.Parser
 
 module Main =
     [<EntryPoint>]
     let main _ =
         // Arb.registerByType (typeof<Marker>.DeclaringType) |> ignore
-//        Check.QuickAll(typeof<Marker>.DeclaringType)
+        // Check.QuickAll(typeof<Marker>.DeclaringType)
         // Check.Quick ``Hex color tag parses``
 
         printfn "%A"
@@ -37,4 +34,5 @@ module Main =
 [*] Item 2
 [/list]
 """
+
         0
